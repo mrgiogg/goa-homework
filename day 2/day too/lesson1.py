091b9b5a45cbac17bf70015bb02590a3600c8b53
mrgiogg <giorgipatashuri92@gmail.com>
2025-02-21 18:30:01 +0400

    first commit

---
+++ day 2/day too/lesson1.py
@@ -0,0 +1,57 @@
+name = 'gio'
+#name aris cvladi
+# = aris cvladis mnishvnelobis mimniwebeli simbolo
+# 'gio' aris cvladis mnishvneloba
+
+
+surname = 'patashuri'
+
+#print funqcias gadaecema ekranze gamotanili obieqti
+
+#print(name)
+
+#print(surname)
+
+name = 'gio'    #es aris STR  (string) tipis cvladi
+
+
+age = ( 'is' +" "+ str(16) + "yo" )#es aris INT (integer) mteli ricxvi
+
+heght = 184 #es aris FLOOT tipis cvladi (atwiladi)
+#boolean (bool)   tipis cvladi
+
+true = 'knows_programing' #true or false 
+
+false = "is_not_skenny" #snakecase ( weris stili standartulad )
+
+isugly = 'false'  #javaskriptuli camelcase
+
+
+print(name + " "+ surname )
+    
+#stringi aris brwyalebshi mocemuli simboloebi
+#print(name + age)
+
+#print(type(age))
+#print(type(name))
+#print(type(surname))
+#print(type(heght))
+#print(type(knows_programing))
+
+
+print(name + ' ' + str(age)) 
+      
+    
+print(name + ' ' + surname + ' ' + str(age) + " " + false + " " + true ) 
+
+
+
+
+
+
+
+
+
+
+
+
