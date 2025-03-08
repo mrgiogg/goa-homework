ad8d9e22389afe6af158164b892d3856f666eab8
mrgiogg <giorgipatashuri92@gmail.com>
2025-03-08 22:51:21 +0400

    commit

---
+++ day 01/class work/class work/true or fals.py
@@ -0,0 +1,13 @@
+num1 = int(input("please_imput_your_age:"))
+num2 = int(input("please_input_your_class_grade:"))
+
+print(num1 > num2)
+print(num2 < num2)
+print(num1 >= num2)
+print(num1 <= num2)
+print(num1 / num2)
+print(num1 * num2)
+print(num1 + num2)
+print(num1 - num2)
+  
+
---
+++ day 01/homework/lesson3.py
@@ -0,0 +1,14 @@
+#i kow this aint practical data but i could not think of sumthing els:)
+#input users age 
+#input users hight 
+num1 = int(input("please enter your age:"))
+
+num2 = int(input("please enter yout hight:"))
+
+
+
+
+print(num1 + num2)
+print(num1 - num2)
+print(num1 / num2)
+print(num1 * num2)
